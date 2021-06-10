8b1fea84b74d0b29fd84e5084bc890495a6ae0fa
Eli Hopkins <ewh3157@rit.edu>
2021-06-09 22:51:48 -0400

    Compatibility with autols

--- pluginrc.zsh
+++ pluginrc.zsh
@@ -1,5 +1,5 @@
-CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
-plugin_name='xxh-plugin-zsh-ohmyzsh'
+CURR_DIR="$(cd "$(dirname "$0" 1>/dev/null)" && pwd)"
+plugin_name='xxh-plugin-zsh-ohmyzsh-autols-fix'
 
 export ZSH="$CURR_DIR/ohmyzsh"
 
